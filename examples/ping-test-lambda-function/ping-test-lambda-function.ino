cf1af1f3c788131c4995cb96915e003e60e05312
Fabian <amazon@fmgrafikdesign.de>
2021-01-15 18:23:35 +0100

    Finished ping-test-lambda-function example

--- examples/ping-test-lambda-function/ping-test-lambda-function.ino
+++ examples/ping-test-lambda-function/ping-test-lambda-function.ino
@@ -9,22 +9,23 @@ void setup() {
   // initialize serial communication at 57600 bits per second:
   Serial.begin(57600);
 
-  // Listen for the reset event
-  WebSerial.on("ping", resetCounter);
+  // We listen for the ping event and define a callback for it.
+  // This time, we use a lambda function, the equivalent to an arrow function in JavaScript.
+  WebSerial.on("ping", [](JSONVar data) {
+    WebSerial.sendEvent("pong");
+  });
 }
 
-void resetCounter(JSONVar param) {
-  reset = millis();
-  WebSerial.send("log", "Counter has been reset!");
-}
+/* The lambda function defined above is the equivalent to this: */
+/*
+ * void sendPongEvent (JSONVar data) {
+ *    WebSerial.sendEvent("pong");
+ * }
+ * 
+ */
 
 // the loop routine runs over and over again forever:
 void loop() {
   // WebSerial needs to check if there is serial data
   WebSerial.check();
-  
-  char str[80];
-  sprintf(str, "It has been %lu ms since the last reset!", millis() - reset);
-  WebSerial.send("log", str);
-  delay(250);        // delay in between reads for stability
 }
