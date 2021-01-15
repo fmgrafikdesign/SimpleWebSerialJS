efb28d39dd5e3753fc0fb624955bd5aa1b0717d0
Fabian <amazon@fmgrafikdesign.de>
2021-01-15 16:19:16 +0100

    Added home-in-on-point-with-2-potentiometers example.

---
+++ examples/home-in-on-point-with-2-potentiometers/home-in-on-point-with-2-potentiometers.ino
@@ -0,0 +1,22 @@
+#include <SimpleWebSerial.h>
+
+SimpleWebSerial WebSerial;
+
+int h, v;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 57600 bits per second:
+  Serial.begin(57600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  h = analogRead(A0);
+  v = analogRead(A1);
+  WebSerial.send("h", map(h, 0, 1023, 1, 49));
+  WebSerial.send("v", map(v, 0, 1023, 1, 49));
+  
+  delay(50);        // delay in between reads for stability
+}
