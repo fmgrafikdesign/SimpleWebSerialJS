1abf7dcb24055c6f6a3dab5d962d4fba194f34e0
Fabian <amazon@fmgrafikdesign.de>
2021-01-20 14:15:19 +0100

    Optimized auto-dark-mode a bit, and added origin trial token as txt file for further reference.

--- examples/auto-dark-mode/auto-dark-mode.ino
+++ examples/auto-dark-mode/auto-dark-mode.ino
@@ -1,8 +1,11 @@
 #include <SimpleWebSerial.h>
 
 SimpleWebSerial WebSerial;
+bool darkmode = false;
+// We use a second bool variable to track changes in darkmode, so we only send events when necessary
+bool previous = false;
 
-// the setup routine runs once when you press reset:
+// the setup routine runs once when you press reset / connect to the Arduino:
 void setup() {
   // initialize serial communication at 57600 bits per second:
   Serial.begin(57600);
@@ -10,10 +13,18 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  bool darkmode = false;
+  // Depending on your LDR, you might have to adjust this threshold.
+  // Use the Serial.print command in the following line to get a feel for the values you get from you LDR while covering / not covering it.
+  // Serial.println(analogRead(0));
   if(analogRead(A0) > 1000) {
     darkmode = true;
+  } else {
+    darkmode = false;
   }
-  WebSerial.send("darkmode", darkmode);
+  
+  if(previous != darkmode) {
+    WebSerial.send("darkmode", darkmode);
+  }
+  previous = darkmode;
   delay(50);        // delay in between reads for stability
 }
---
+++ examples/control-rgb-led-with-sliders/control-rgb-led-with-sliders.ino
@@ -0,0 +1,110 @@
+#include <SimpleWebSerial.h>
+
+// These constants won't change. They're used to give names to the pins used:
+const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogOutPin = 9; // Analog output pin that the LED is attached to
+
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+
+int r = 4;
+int g = 5;
+int b = 3;
+
+char rgb[] = {0, 0, 0};
+long number = 0;
+SimpleWebSerial WebSerial;
+
+void function1(JSONVar data) {
+  Serial.println("Function 1 called with parameters");
+  Serial.println(JSON.stringify(data));
+  // WebSerial.send("hexadecimal", "whatever");
+}
+
+void function2(JSONVar data) {
+  Serial.println("Function 2 called");
+}
+
+void function3(JSONVar data) {
+  Serial.println("Function 3 called");
+}
+
+void setup() {
+  // initialize serial communications
+  Serial.begin(57600);
+  pinMode(r, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(b, OUTPUT);
+
+  // Let's try some lambda magic - works
+  WebSerial.on("lambda", [](JSONVar data) {
+    WebSerial.send("vialambda", "some params");
+  });
+  WebSerial.on("ledon", (JSONVar data) {
+  Serial.println("Function 2 called");
+});
+
+
+  WebSerial.on("values", setLed);
+
+  WebSerial.on("parse", [](JSONVar data) {
+    Serial.println(JSON.parse(data));
+    Serial.println(JSON.typeof(JSON.parse(data)));
+  });
+
+  WebSerial.on("goodone", [](JSONVar data) {
+    Serial.println("event good one called!");
+  });
+
+  WebSerial.on("data", [](JSONVar data) {
+    Serial.println(JSON.stringify(data));
+  });
+
+  //WebSerial.send("hello there!");
+  //WebSerial.setCallback(function1);
+  // WebSerial.listEvents();
+}
+
+
+void setLed(JSONVar values) {
+  //Serial.println(JSON.stringify(values));
+  // if you're using a common-cathode LED, just use "constrain(color, 0, 255);"
+  int red = 255 - constrain((int)values["r"], 0, 255);
+  int green = 255 - constrain((int)values["g"], 0, 255);
+  int blue = 255 - constrain((int)values["b"], 0, 255);
+
+  // fade the red, green, and blue legs of the LED:
+  analogWrite(r, red);
+  analogWrite(g, green);
+  analogWrite(b, blue);
+
+  // print the three numbers in one string as hexadecimal:
+  char format[8];
+  sprintf(format, "#%02X%02X%02X", (int)values["r"], (int)values["g"], (int)values["b"]);
+  WebSerial.send("hexadecimal", format);
+  //WebSerial.sendEvent("hexadecimal");
+  // WebSerial.send("hexadecimal", values);
+
+/*
+Serial.print(JSON.typeof(values["r"]));
+  Serial.print('+');
+  Serial.print(JSON.typeof(values["g"]));
+  Serial.print('+');
+  Serial.println(JSON.typeof(values["b"]));
+*/
+  /*
+  Serial.print(red);
+  Serial.print('+');
+  Serial.print(green);
+  Serial.print('+');
+  Serial.println(blue);
+  */
+}
+
+
+void loop() {
+  // Serial.println("running loop");
+  WebSerial.check();
+  number++;
+  delay(2);
+}
